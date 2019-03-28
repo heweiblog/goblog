867af24b302eb224f166e42b32f8fb5fb63ca468
heweiwei <ww.he@yamutech.com>
2019-03-28 17:45:30 +0800

    add deamon

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
+	"github.com/sevlyar/go-daemon"
 	"goblog/models"
 	_ "goblog/routers"
+	"log"
+	"os"
 )
 
 func init() {
@@ -12,7 +15,29 @@ func init() {
 }
 
 func main() {
-	orm.Debug = true
+	if 2 == len(os.Args) && os.Args[1] == "-b" {
+		cntxt := &daemon.Context{
+			PidFileName: "goblog_pid",
+			PidFilePerm: 0644,
+			LogFileName: "goblog.log",
+			LogFilePerm: 0640,
+			WorkDir:     "./",
+			Umask:       027,
+			Args:        []string{"[goblog]"},
+		}
+
+		d, err := cntxt.Reborn()
+		if err != nil {
+			log.Fatal("Unable to run: ", err)
+		}
+		if d != nil {
+			return
+		}
+		defer cntxt.Release()
+	}
+
+	//orm.Debug = true
+	orm.Debug = false
 	orm.RunSyncdb("default", false, true)
 	beego.Run()
 }
